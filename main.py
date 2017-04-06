c245371738a813736ef8a5ddc1ec61168b083cb6
Dagofcode <dagoberto.rodriguez02@utrgv.edu>
2017-04-06 13:21:09 -0500

    game uploaded with map and formatting

--- main.py
+++ main.py
@@ -1,3 +1,90 @@
+import pygame, sys
+from settings import *
+
+class Tile:
+    def __init__(self, screen, pointx, pointy, x,y):
+        self.pointx = pointx
+        self.pointy = pointy
+        self.size = tile_size
+        self.isActive = True
+        self.screen = screen
+        self.x = x
+        self.y = y
+
+    def draw_tile(self):
+        #pygame.draw.Rect(self.screen, self.color1, self.size /2, self.size/2)
+        pygame.draw.rect(self.screen, rect_color[self.x], (self.pointx, self.pointy, self.size, self.size))
+        pygame.draw.ellipse(self.screen, circle_color[self.y], (self.pointx, self.pointy, self.size / 2, self.size / 2))
+
+
+class Game:
+    def __init__(self):
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((screen_width, screen_height))
+        self.running = True
+        self.tile = []
+
+
+
+
+    def draw_grid(self):
+
+
+        for x in range(board_width):
+            for y in range(board_width):
+                startx = (x * tile_size) + XMARGIN
+                starty = (y * tile_size) + YMARGIN
+                endy = YMARGIN + (board_height * tile_size)
+
+                self.tile.append( Tile(self.screen, startx,starty, x,y))
+
+        for tile in range(len(self.tile)):
+            self.tile[tile].draw_tile()
+
+
+
+
+
+        for x in  range(board_width + 1):
+            startx = (x * tile_size) + XMARGIN
+            starty = YMARGIN
+            endx = (x * tile_size) + XMARGIN
+            endy = YMARGIN + (board_height * tile_size)
+
+
+            pygame.draw.line(self.screen, WHITE, (startx, starty), (endx, endy))
+        for y in  range(board_height + 1):
+            startx = XMARGIN
+            starty = (y * tile_size) + YMARGIN
+            endx = XMARGIN + (board_width * tile_size)
+            endy = (y * tile_size) + YMARGIN
+            pygame.draw.line(self.screen, WHITE, (startx, starty), (endx, endy))
+
+        #draw tiles
+
+
+
+
+    def events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                self.running = False;
+
+    def run(self):
+        while self.running:
+            self.screen.fill(BLACK)
+            self.draw_grid()
+            self.events()
+            pygame.display.flip()
+
+
+
+
+game = Game()
+game.run()
+
+'''
 import pygame
 import sys
 from functions import *
@@ -29,3 +116,6 @@ while running:
 
     if pygame.mouse.get_pressed() == (1,0,0):
         print(pygame.mouse.get_pos())
+
+
+'''
--- settings.py
+++ settings.py
@@ -1,10 +1,26 @@
-# Screen size
-SCREEN_WIDTH = 700
-SCREEN_HEIGHT = 400
+screen_width = 600
+screen_height = 600
+board_width = 4
+board_height = 4
+tile_size = 100
 
-# menu
-MENU_TITLE = "NIYA"
 
-# color triples
-BLACK = (0,0,0)
-WHITE = (255,255,255)
+XMARGIN = int((screen_width - (board_width * tile_size)) / 2)
+YMARGIN = int((screen_height - (board_height * tile_size)) / 2)
+
+#colors
+WHITE = (255, 255, 255)
+BLACK = (  0,   0,   0)
+
+RED = (255,0,0)
+GREEN = (  0, 155,   0)
+BLUE = (0,0,255)
+YELLOW = (255,255,0)
+PURPLE = (128,0,128)
+ORANGE = (255,165,0)
+CYAN = (0,255,255)
+PINK = (255,192,203)
+
+
+rect_color = [RED, GREEN, BLUE, YELLOW]
+circle_color = [PURPLE, BLACK, CYAN, PINK]
